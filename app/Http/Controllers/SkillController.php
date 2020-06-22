79039ff2f726c8ca9d1efc79cf7c6c360bc14ff6
Ngiyabonga <kealebogagakenne@gmail.com>
2020-06-22 16:40:47 +0200

    update v1.2

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -7,16 +7,22 @@ use App\Course;
 use App\Skill;
 use Auth;
 use DB;
-use Validator,Redirect,Response,File;
+use Validator,Redirect,Response;
 Use App\Image;
-
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 class SkillController extends Controller
 {
     //
     public function addSkills(Request $request){
         
         $course = new Course;
-        
+        $image = new Image;
+       
+        $proof = $request->file('proof');
+        $extension = $proof->getClientOriginalExtension();
+        Storage::disk('public')->put($proof->getFilename().'.'.$extension,  File::get($proof));
+    
         /* Store Skill Details*/ 
         $course->user_id = Auth::id();
         $course->name = $request->name;
@@ -24,6 +30,10 @@ class SkillController extends Controller
         $course->level = $request->level;
         $course->status = false;
         $course->save();
+
+        $image->link = $proof->getFilename().'.'.$extension;
+        $image->user_id = Auth::id();
+        $image->save();
 
         return view('Adept.index')->with('success', 'Skill added');
     }
